16424a1d91eb5933f197cf56afba9151f77044e8
Bazanova Nadezhda <bazanovanv@gmail.com>
2019-08-13 18:02:03 +0300

    Работа над ScrollViewer

--- Try/MainWindow.xaml.cs
+++ Try/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,16 +21,44 @@ namespace Try
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<string> ObsString { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ObsString = new ObservableCollection<string>()
+            {
+                "LG Nexus 5X",
+                "Huawei Nexus 6P",
+                "iPhone 6S",
+                "iPhone 6S Plus",
+                "Аsus Zenphone 2",
+                "Microsoft Lumia 950",
+                "LG Nexus 5X",
+                "Huawei Nexus 6P",
+                "iPhone 6S",
+                "iPhone 6S Plus",
+                "Аsus Zenphone 2",
+                "Microsoft Lumia 950",
+                "LG Nexus 5X",
+                "Huawei Nexus 6P",
+                "iPhone 6S",
+                "iPhone 6S Plus",
+                "Аsus Zenphone 2",
+                "Microsoft Lumia 950",
+
+            };
+
+            DataContext = this;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            ResourceDictionary newDictionary = new ResourceDictionary();
-            newDictionary.Source = new Uri("Style2.xaml", UriKind.Relative);
-            this.Resources.MergedDictionaries[0] = newDictionary;
+            //ResourceDictionary newDictionary = new ResourceDictionary();
+            //newDictionary.Source = new Uri("Style2.xaml", UriKind.Relative);
+            //this.Resources.MergedDictionaries[0] = newDictionary;
+
             //Application.Current.Resources.MergedDictionaries[0] = newDictionary;
         }
     }
